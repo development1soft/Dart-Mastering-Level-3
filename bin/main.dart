f6d416880240248bec0ef1fdb65da168c2a94a83
Eyad Web Developmnet <30596418+development1soft@users.noreply.github.com>
2020-06-20 10:14:05 +0300

    Initial Project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_mastering_course/dart_mastering_course.dart' as dart_mastering_course;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_mastering_course.calculate()}!');
+}
---
+++ test/dart_mastering_course_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_mastering_course/dart_mastering_course.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
