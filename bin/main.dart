ff97751900c68a265a9e2e4a42a038ecc841dbad
Eyad Web Developmnet <30596418+development1soft@users.noreply.github.com>
2020-06-23 23:50:37 +0300

    Dart Level 3 Finished Successfully

--- bin/main.dart
+++ bin/main.dart
@@ -1,17 +1,111 @@
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+void main(){
+  
+  // getdata();
+  
+  // storePost();
 
-main() async{
+  // deletePost();
 
-  Future<String> getData() async{
-    return await Future.delayed(Duration(seconds: 5),(){
-      return 'Welcome Dart';
-    });
+  // updateFullPost();
+
+  // updatePartPost();
+
+}
+
+getdata() async{
+
+  var url = "https://jsonplaceholder.typicode.com/users";
+
+  var response = await http.get(url);
+
+  var responseJson = convert.jsonDecode(response.body);
+
+  for (var user in responseJson) {
+    print(user['email']);
   }
 
-  await getData().then((value){
-    print(value);
+}
+
+storePost() async{
+    
+  var url = "https://jsonplaceholder.typicode.com/posts";
+
+  var response = await http.post(
+    url,
+    body : {'user_id':'25635','id':'3000','title':'first title from api','body':'first body from api'}
+  );
+
+  var responseJson = convert.jsonDecode(response.body);
+
+  // 200 ok
+
+  if(response.statusCode == 201)
+  {
+    
+    print(responseJson['title'].toString());
+  
+  }else{
+
+    print('please try later');
+  
+  }
+
+}
+
+deletePost() async{
+
+  var url = "https://jsonplaceholder.typicode.com/posts/1";
+
+  var response = await http.delete(url);
+
+  if(response.statusCode == 200)
+  {
+    print('post deleted successfully');
+
+  }else{
+
+    print('please try later');
+
+  }
+
+}
+
+updateFullPost() async{
+
+  var url = "https://jsonplaceholder.typicode.com/posts/1";
+
+  var response = await http.put(url,body: {
+  
+    'userId':'2',
+  
+    'title':'test update title',
+  
+    'body':'test update body',
+  
   });
 
-  print('welcome');
+  var jsonResponse = convert.jsonDecode(response.body);
+
+  print(jsonResponse['title'].toString());
+
+
+}
+
+updatePartPost() async{
+
+  var url = "https://jsonplaceholder.typicode.com/posts/1";
+
+  var response = await http.patch(url,body: {
+  
+    'title':'test update title using patch',
+  
+  });
+
+  var jsonResponse = convert.jsonDecode(response.body);
+
+  print(jsonResponse);
+
 
 }
---
+++ lib/future.dart
@@ -0,0 +1,11 @@
+  // Future<String> getData() async{
+  //   return await Future.delayed(Duration(seconds: 5),(){
+  //     return 'Welcome Dart';
+  //   });
+  // }
+
+  // await getData().then((value){
+  //   print(value);
+  // });
+
+  // print('welcome');
